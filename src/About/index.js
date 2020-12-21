f1342ee3a424dba7857509cac72f482a4e29ffe3
Jordan Carver <jordan_carver@hotmail.com>
2020-12-20 19:14:48 -0700

    added About section

---
+++ src/About/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="my-5">
+            <h1 id="about">Want to know more about me?</h1>
+            <div className="my-2">
+                <p>
+                    I am a stay-at-home dad learning to code with amazing kids, spouse, and big dog!
+
+                    I love ...
+                        ... soccer, long boarding, roller blading, hiking, swimming ...
+                        ... culture, languages, music, art, reading, traveling ....
+                        ... outdoors, sunsets, fresh air, beaches, mountains ...
+                        ... cooking, drinking, eating food
+                    
+                    I work as a ...
+                        ... consultant, adjunct professor, and coach.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import About from './components/About';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: '',
+      description: '',
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About />
+          </>
+        ) : (
+          <ContactForm />
+        )}
+      </main>
     </div>
   );
 }
+
+export default App;
 
 export default App;
